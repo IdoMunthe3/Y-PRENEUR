b219c3c0950e8fb053875cc1cc0f2bf052f5195f
IdoMunthe3 <idomunthe52@student.ub.ac.id>
2022-03-19 00:06:38 +0700

    connecting sign up activity to home activity

--- app/src/main/java/com/example/y_preneur/RegisterActivity.kt
+++ app/src/main/java/com/example/y_preneur/RegisterActivity.kt
@@ -1,12 +1,22 @@
 package com.example.y_preneur
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.myapplication.HomeActivity
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val homepageBtn: Button = findViewById (R.id.btn_create)
+
+        homepageBtn.setOnClickListener() {
+            val intent = Intent(this@RegisterActivity, HomeActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
